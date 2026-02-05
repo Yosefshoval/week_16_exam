60aaeb4e7dcea0b3fbf98092c14dcb8fbb74d1e5
Yosef shoval <ys0542522504@gmail.com>
2026-02-05 11:03:05 +0200

    add env to mongo.yaml, add username ans password for mongoclient connection

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ RUN pip install -r requirements.txt
 
 COPY ./app .
 
+COPY employee_data_advanced.json .
+
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/connection.py
+++ app/connection.py
@@ -2,11 +2,12 @@ import json
 from pymongo import MongoClient
 from os import getenv
 
-
-mongo_uri = getenv("MONGO_URI", "mongodb://localhost:27017/")
+MONGO_USER = getenv('MONGO_INITDB_ROOT_USERNAME')
+MONGO_PASS = getenv('MONGO_INITDB_ROOT_PASSWORD')
+mongo_uri = getenv("MONGO_URI", "mongodb://silent-payroll-mongo-svc:27017/")
 mongo_db = getenv("MONGO_DB", "testdb")
 mongo_collection = getenv("MONGO_COLLECTION", "testcollection")
-file_path = r'C:\Users\menachem shoval\Desktop\Kodcode\tarining\tests_analiza\week_16_exam\employee_data_advanced.json'
+file_path = r'employee_data_advanced.json'
 
 print('# Making Connection')
 myclient = MongoClient(mongo_uri)
@@ -14,8 +15,9 @@ myclient = MongoClient(mongo_uri)
 print('# database')
 db = myclient[mongo_db]
 
+db.authenticate(MONGO_USER, MONGO_PASS)
+
 print('# Created or Switched to collection')
-# names: GeeksForGeeks
 Collection = db[mongo_collection]
 
 print('# Loading or Opening the json file')
