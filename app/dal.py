796216a90da19870583b59cc52baa8ddce354a5e
Yosef shoval <ys0542522504@gmail.com>
2026-02-05 09:44:19 +0200

    write the 1st question, convert id to str

---
+++ app/dal.py
@@ -0,0 +1,28 @@
+from connection import Collection
+
+def conver_id_to_string(doc):
+    if not doc:
+        return None
+    if "_id" in doc:
+        doc["_id"] = str(doc["_id"])
+
+def convert_multiple_doc(docs):
+    return [conver_id_to_string(doc) for doc in docs]
+
+
+def get_engineering_high_salary_employees():
+    result = Collection.find(
+        {'$and' [
+            {'job_role.department' : 'Engineering'}, 
+            {'salary': {'$gt':65000}}
+            ]},
+        {'employee_id': 1, 'name': 1, 'salary' : 1}
+        )
+    result = convert_multiple_doc(result)
+    return result
+
+
+
+    
+
+    
