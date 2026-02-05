9ce2b7c7fff3ed1b00edfb630304991f6d26dd7b
Yosef shoval <ys0542522504@gmail.com>
2026-02-05 11:41:13 +0200

    change mongo uri, add dockerignore

--- app/connection.py
+++ app/connection.py
@@ -2,20 +2,25 @@ import json
 from pymongo import MongoClient
 from os import getenv
 
-MONGO_USER = getenv('MONGO_INITDB_ROOT_USERNAME')
-MONGO_PASS = getenv('MONGO_INITDB_ROOT_PASSWORD')
-mongo_uri = getenv("MONGO_URI", "mongodb://silent-payroll-mongo-svc:27017/")
+MONGO_USER = getenv('MONGO_INITDB_ROOT_USERNAME', 'root')
+MONGO_PASS = getenv('MONGO_INITDB_ROOT_PASSWORD', 'password')
+mongo_uri = getenv("MONGO_URI", "localhost")
 mongo_db = getenv("MONGO_DB", "testdb")
 mongo_collection = getenv("MONGO_COLLECTION", "testcollection")
 file_path = r'employee_data_advanced.json'
 
 print('# Making Connection')
-myclient = MongoClient(mongo_uri)
+myclient = MongoClient(
+    host=mongo_uri,
+    port=27017,
+    username=MONGO_USER,
+    password=MONGO_PASS
+)
+myclient.admin.command('ping')
+print(myclient)
 
 print('# database')
 db = myclient[mongo_db]
-
-db.authenticate(MONGO_USER, MONGO_PASS)
 
 print('# Created or Switched to collection')
 Collection = db[mongo_collection]
