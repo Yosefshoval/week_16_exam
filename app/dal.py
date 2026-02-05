896ca9e5a8492ac8668ae1e66eb979270e3a9242
Yosef shoval <ys0542522504@gmail.com>
2026-02-05 09:50:20 +0200

    write the 2sc question

--- app/dal.py
+++ app/dal.py
@@ -23,6 +23,20 @@ def get_engineering_high_salary_employees():
 
 
 
-    
+def get_employees_by_age_and_role():
+    result = Collection.find(
+        {'$and' [
+            {{'age' : {'$gte' : 30}}, 
+             {'age' : {'$lte' : 45}}
+             }, 
+            {'$or' : [
+                {'job_role.department' : 'Engineer'}, 
+                {'job_role.department' : 'Specialist'}
+                ]}
+            ]},
+        {'employee_id': 1, 'name': 1, 'salary' : 1}
+        )
+    result = convert_multiple_doc(result)
+    return result
 
     
